82742eff0e86775d3fe7e126d9420039b9ff3bea
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2022-05-15 12:54:50 +0200

    Add Human class

---
+++ src/main/java/Human.java
@@ -0,0 +1,14 @@
+import org.javatuples.Pair;
+
+public class Human extends Creature {
+    private int teamID;
+    private boolean onExpedition;
+    private Village parentVillage;
+
+    public Human(int teamID, Map parentMap, Village parentVillage, Pair<Integer, Integer> position) {
+        super(CreatureType.HUMAN, parentMap, position);
+    }
+    public boolean isOnExpedition() {return onExpedition;}
+    public void goToExpedition() {}
+    public Village getParentVillage() {return parentVillage;}
+}
