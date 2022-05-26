b44db7781a41067dc6ae570854759b77d7453574
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2022-05-26 23:16:47 +0200

    Add MainWindow

---
+++ src/main/java/MainWindow.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+
+public class MainWindow extends JFrame {
+    final int windowSize;
+    final HeightMap heightMap;
+    final BufferedImage bufferedImage;
+    final WritableRaster writableRaster;
+
+    public MainWindow(int windowSize, HeightMap heightMap) {
+        this.windowSize = windowSize;
+        this.heightMap = heightMap;
+        bufferedImage = new BufferedImage(windowSize, windowSize,
+                BufferedImage.TYPE_3BYTE_BGR);
+        writableRaster = bufferedImage.getRaster();
+        setTitle("Procedural Civilization");
+        setSize(windowSize, windowSize);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        for (int i = 0; i < windowSize; i++) {
+            for (int j = 0; j < windowSize; j++) {
+                writableRaster.setPixel(i, j, BiomeConverter.BiomeColors.get(
+                        BiomeConverter.getBiome(heightMap.height[i][j])));
+            }
+        }
+        g.drawImage(bufferedImage, 0,0, null);
+    }
+}
