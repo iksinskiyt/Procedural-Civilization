1a112e76bce3b02bef8ce1bd88f6f98048468d8c
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2022-05-26 23:16:23 +0200

    Move Biome enum to BiomeConverter; add Perlin integration

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -2,20 +2,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Map {
-    public enum Biome {
-        OCEAN,
-        PLAINS,
-        FOREST,
-        MOUNTAINS
-    }
-
     private final List<Village> villages;
     private final HeightMap heightMap;
     private final List<Creature> creatures;
     private boolean simulationComplete;
 
     private Position getRandomPosition(
-            List<Biome> allowedBiomes) {
+            List<BiomeConverter.Biome> allowedBiomes) {
         // TODO: Replace with real random position generation
         return new Position(0, 0);
     }
@@ -24,20 +17,20 @@ public class Map {
                SimulationOptions simulationOptions) {
         Perlin perlin = new Perlin(perlinOptions);
         heightMap = new HeightMap();
-        // TODO: Perlin generation
+        heightMap.height = perlin.generatePerlinNoise2D();
 
         villages = new ArrayList<>();
 
         for (int i = 0; i < simulationOptions.nTeams; i++) {
             Village village =
-                    new Village(getRandomPosition(List.of(Biome.PLAINS)), i,
-                            this);
+                    new Village(getRandomPosition(
+                            List.of(BiomeConverter.Biome.PLAINS)), i, this);
             villages.add(village);
 
             for (int j = 0; j < simulationOptions.teamPopulation; j++) {
                 village.addVillager(new Human(i, this, village,
                         getRandomPosition(
-                                List.of(Biome.PLAINS, Biome.FOREST))));
+                                List.of(BiomeConverter.Biome.PLAINS))));
             }
         }
 
@@ -45,12 +38,14 @@ public class Map {
 
         for (int i = 0; i < simulationOptions.nCows; i++) {
             creatures.add(
-                    new Cow(this, getRandomPosition(List.of(Biome.PLAINS))));
+                    new Cow(this, getRandomPosition(
+                            List.of(BiomeConverter.Biome.PLAINS))));
         }
 
         for (int i = 0; i < simulationOptions.nHamsters; i++) {
             creatures.add(new Hamster(this,
-                    getRandomPosition(List.of(Biome.MOUNTAINS))));
+                    getRandomPosition(
+                            List.of(BiomeConverter.Biome.MOUNTAINS))));
         }
     }
 
