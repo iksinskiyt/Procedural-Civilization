a8a4310bc9de53de9fb8cdcd1d9da5ac02e83b17
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2022-05-27 20:58:34 +0200

    Make the map image drawn only once and prepare to draw entities

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -2,19 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
+import java.util.List;
 
 public class MainWindow extends JFrame {
-    final int windowSize;
-    final HeightMap heightMap;
-    final BufferedImage bufferedImage;
-    final WritableRaster writableRaster;
+    private final int windowSize;
+    private final Map map;
+    private final HeightMap heightMap;
+    private final List<Creature> creatures;
+    private final List<Village> villages;
+    private final BufferedImage mapImage;
 
-    public MainWindow(int windowSize, HeightMap heightMap) {
+    public MainWindow(int windowSize, Map map) {
         this.windowSize = windowSize;
-        this.heightMap = heightMap;
-        bufferedImage = new BufferedImage(windowSize, windowSize,
+        this.map = map;
+
+        // Get all necessary references
+        this.heightMap = map.getHeightMap();
+        this.creatures = map.getCreatureList();
+        this.villages = map.getVillageList();
+
+        // Draw a map image
+        mapImage = new BufferedImage(windowSize, windowSize,
                 BufferedImage.TYPE_3BYTE_BGR);
-        writableRaster = bufferedImage.getRaster();
+        WritableRaster mapImageRaster = mapImage.getRaster();
+        for (int i = 0; i < windowSize; i++) {
+            for (int j = 0; j < windowSize; j++) {
+                mapImageRaster.setPixel(i, j,
+                        BiomeConverter.getBiomeColor(heightMap.height[i][j]));
+            }
+        }
+
         setTitle("Procedural Civilization");
         setSize(windowSize, windowSize);
         setResizable(false);
@@ -23,12 +40,6 @@ public class MainWindow extends JFrame {
 
     @Override
     public void paint(Graphics g) {
-        for (int i = 0; i < windowSize; i++) {
-            for (int j = 0; j < windowSize; j++) {
-                writableRaster.setPixel(i, j,
-                        BiomeConverter.getBiomeColor(heightMap.height[i][j]));
-            }
-        }
-        g.drawImage(bufferedImage, 0, 0, null);
+        g.drawImage(mapImage, 0, 0, null);
     }
 }
