6ef79504d4513be025198a24c03edcbbe515c31c
adrianmatkowski01 <adrianmatkowski01@gmail.com>
2022-05-29 23:24:50 +0200

    Added note with explanation

--- src/main/java/Building.java
+++ src/main/java/Building.java
@@ -9,7 +9,7 @@ public class Building {
     public int buildCost;
     public Item neededMaterial;
 
-    public Building(Item neededMaterial, int buildCost){
+    public Building(Item neededMaterial, int buildCost){ // for right now (by for now i mean forever), its done poorly, object thinks the only material its made off is wood, but while creation, it uses other materials also
         this.buildCost = buildCost;
         this.neededMaterial = neededMaterial;
     }
