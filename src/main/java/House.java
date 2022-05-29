f48a964502afdf9f949d894ee7291fb433fb0d94
adrianmatkowski01 <adrianmatkowski01@gmail.com>
2022-05-29 22:51:13 +0200

    Added a variable house cost

--- src/main/java/House.java
+++ src/main/java/House.java
@@ -1,5 +1,7 @@
 public class House extends Building {
     
+    public static int houseCost = 25; // changable house cost variable
+
     public House(Item neededMaterial, int buildCost){
         super(neededMaterial, buildCost);
     }
@@ -7,7 +9,7 @@ public class House extends Building {
     public static House createHouse(Inventory inventory){
         Item.ItemType itemType = Item.ItemType.WOOD;
         Item item = new Item(itemType);
-        House house = new House(item, 25);
+        House house = new House(item, houseCost);
         if (inventory.useItem(item, house.buildCost)){
             return house;
         }
