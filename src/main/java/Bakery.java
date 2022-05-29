c52169d75123fb536094ad14e1d0e0295e43443d
adrianmatkowski01 <adrianmatkowski01@gmail.com>
2022-05-30 01:43:07 +0200

    Bakery variables

--- src/main/java/Bakery.java
+++ src/main/java/Bakery.java
@@ -1,6 +1,8 @@
 public class Bakery extends Building {
     // Yes, the bakery bakes bread and meat... don't ask
     public static int bakeryStoneCost = 20;
+    public static int meatCost = 5;
+    public static int breadCost = 10 ;
 
     public Bakery(Item neededMaterial, int buildCost){
         super(neededMaterial, buildCost);
@@ -19,11 +21,11 @@ public class Bakery extends Building {
     public static void produceFood(Inventory inventory){
         Item meat = new Item(Item.ItemType.MEAT);
         Item bread = new Item(Item.ItemType.WHEAT);
-        if(inventory.useItem(meat, 5)){ // why does it use 5 meat to make 1 food? don't ask me, i have no idea, its 01:30AM and it's gotta be optimized somehow... maybe the meat from ladybugstore has a fuckton of water inside
+        if(inventory.useItem(meat, meatCost)){ // why does it use 5 meat to make 1 food? don't ask me, i have no idea, its 01:30AM and it's gotta be optimized somehow... maybe the meat from ladybugstore has a fuckton of water inside
             Item food = new Item(Item.ItemType.FOOD); // i hope me swearing in those commits and comment's won't affect our final grade lol
             inventory.addItem(food, 1); // why the hell does the bakery bake meat?????? what????????
         }
-        if(inventory.useItem(bread, 10)){ 
+        if(inventory.useItem(bread, breadCost)){ 
             Item food = new Item(Item.ItemType.FOOD);
             inventory.addItem(food, 1);
         }
