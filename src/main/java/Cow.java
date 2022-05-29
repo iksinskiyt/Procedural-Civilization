fec806d7d1756b4add0149f7b2c19bf7e57a262a
adrianmatkowski01 <adrianmatkowski01@gmail.com>
2022-05-29 20:40:20 +0200

    Fixed addItem method

--- src/main/java/Cow.java
+++ src/main/java/Cow.java
@@ -1,6 +1,6 @@
 public class Cow extends Creature {
     public Cow(Map parentMap, Position position) {
         super(parentMap, position, 20, 0, 2, 0);
-        inventory.addItem(new Item(Item.ItemType.FOOD, 4));
+        inventory.addItem(new Item(Item.ItemType.FOOD), 4);
     }
 }
