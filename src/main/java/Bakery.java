77f9f6e431d077a56c7a115197199d1dd1916a37
adrianmatkowski01 <adrianmatkowski01@gmail.com>
2022-05-29 23:54:57 +0200

    Expanded bakery class

--- src/main/java/Bakery.java
+++ src/main/java/Bakery.java
@@ -1,3 +1,19 @@
-public class Bakery {
-    
+public class Bakery extends Building {
+
+    public static int bakeryStoneCost = 20;
+
+    public Bakery(Item neededMaterial, int buildCost){
+        super(neededMaterial, buildCost);
+    }
+
+    public static Bakery createBakery(Inventory inventory){
+        Item.ItemType stone = Item.ItemType.STONE;
+        Item stoneItem = new Item(stone);
+        Bakery bakery = new Bakery(stoneItem, bakeryStoneCost);
+        if (inventory.useItem(stoneItem, bakeryStoneCost)){
+            return bakery;
+        }
+        else return null;
+    }
+
 }
