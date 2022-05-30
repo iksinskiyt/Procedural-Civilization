4104d8d26b2c43708be5e11163c0b90e525dd4bb
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2022-05-30 18:13:47 +0200

    Add a distance calculation method

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -6,4 +6,11 @@ public class Position {
         this.x = x;
         this.y = y;
     }
+
+    static int squaredDistanceBetween(Position p1, Position p2)
+    {
+        int deltaX = p1.x - p2.x;
+        int deltaY = p1.y - p2.y;
+        return deltaX*deltaX + deltaY*deltaY;
+    }
 }
