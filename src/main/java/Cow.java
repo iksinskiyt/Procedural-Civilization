91558031e04b680d05753af6b3e82b4d27c869f4
Hubert Wiśniewski <263815@student.pwr.edu.pl>
2022-06-01 11:36:02 +0200

    Increase inventory sizes of Cow and Hamster

--- src/main/java/Cow.java
+++ src/main/java/Cow.java
@@ -1,7 +1,7 @@
 public class Cow extends Creature {
     public Cow(Map parentMap, Position position) {
-        super(parentMap, position, 20, 0, 2, 0);
-        inventory.addItem(new Item(Item.ItemType.MEAT), 4);
-        inventory.addItem(new Item(Item.ItemType.LEATHER), 2);
+        super(parentMap, position, 20, 0, 2, 6);
+        inventory.addItem(Inventory.ItemType.MEAT, 4);
+        inventory.addItem(Inventory.ItemType.LEATHER, 2);
     }
 }
